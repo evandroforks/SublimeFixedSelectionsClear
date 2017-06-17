1ac9a4eeda67e274563f2ae63ed6cfa5ff4c98b7
evandrocoan <evandrocoan@hotmail.com>
2017-06-17 11:44:42 -0300

    Added initial files.

---
+++ fixed_clear_selections.py
@@ -0,0 +1,15 @@
+
+
+
+import sublime
+import sublime_plugin
+
+
+class FixedClearSelectionsCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+
+        # print( 'Calling FixedClearSelections...' )
+        self.view.sel().clear()
+
+
